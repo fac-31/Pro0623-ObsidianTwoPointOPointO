0c42f6d6526196750bf42aab2ac8e2e60cb88adb
Red Hellier <redhellier@gmail.com>
2025-07-09 09:45:55 +0100

    Playwright setup (#37)
    * Moved Neo4j config and driver set-up into dedicated db folder in lib and updated imports where necessary.
    
    * Removed unused props.
    
    * Can Login
    
    * Merge changes fixed
    
    * AuthForm.svelte added, component that can be abstracted to any form - formData value ts issues to add
    
    * Signup working but still accepts a duplicate email on sign up
    
    * Log In and Sign Up forms work using AuthForm component
    
    * packages installed and merges edited
    
    * Styling and Accessibility checks
    
    * env added to workflows and secrets
    
    * throw redirects
    
    * Vitest installed and configured, test written for AuthFor Component using Gemini CLI
    
    * Playwright setup with workflow and example tests
    
    * Example tests written and webserver configuered
    
    * vitest configured in vite.config.ts
    
    * Linted and envs added
    
    ---------
    
    Co-authored-by: chrislush <133811141+chrislush@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { svelteTesting } from '@testing-library/svelte/vite';
+import { defineConfig } from 'vitest/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()]
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson(), svelteTesting()],
+	test: {
+		include: ['src/test/component/**/*.test.ts'],
+		environment: 'jsdom',
+		setupFiles: ['./vitest-setup.ts']
+	},
+	resolve: process.env.VITEST
+		? {
+				conditions: ['browser']
+			}
+		: undefined
 });
