c9d3a11aea12c99631db0d4df88e4534fa5bdd1d
nchua3012 <n.chua3012@googlemail.com>
2025-07-19 11:43:51 +0100

    Added public graph view component, search bar component and changes to floating button for accessibility

---
+++ src/lib/components/PublicGraphView.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import SearchBar from './SearchBar.svelte';
+	import GraphView from './GraphView.svelte';
+	import type { GraphData } from '$lib/types/graph';
+
+	export let graphData: GraphData;
+</script>
+
+<div class="w-screen h-screen flex flex-col">
+	<SearchBar />
+	<div class="flex-1">
+		<GraphView {graphData} />
+	</div>
+</div>
---
+++ src/lib/components/SearchBar.svelte
@@ -0,0 +1,3 @@
+<div class="w-full p-4">
+  <input type="search" placeholder="Search..." class="input input-bordered w-full" />
+</div>
