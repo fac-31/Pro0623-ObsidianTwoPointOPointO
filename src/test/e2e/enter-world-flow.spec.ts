de5c9497f5e720f6ab74ae5fa2551a1a321728c7
Richard Couzens <richardcouzens96@gmail.com>
2025-07-20 06:24:52 +0100

    test: changed urls to relative

--- src/test/e2e/enter-world-flow.spec.ts
+++ src/test/e2e/enter-world-flow.spec.ts
@@ -1,9 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-test('Clicking on a world and then the enter-world button takes you to that worlds page', async ({
+test('Clicking on The Verdant Expanse and then the enter-world button takes you to that worlds page', async ({
 	page
 }) => {
-	await page.goto('http://localhost:5173/worlds');
+	await page.goto('/worlds');
 
 	await page
 		.locator('canvas')
@@ -16,6 +16,6 @@ test('Clicking on a world and then the enter-world button takes you to that worl
 		});
 	await expect(page.getByRole('heading', { name: 'The Verdant Expanse' })).toBeVisible();
 	await page.getByRole('button', { name: 'Enter World' }).click();
-	await page.goto('http://localhost:5173/worlds/4:1e2fd644-54fb-4048-9d76-adc63b6fcd5b:13');
+	await page.goto('/worlds/4:1e2fd644-54fb-4048-9d76-adc63b6fcd5b:13');
 	await expect(page.getByRole('heading', { name: 'The Verdant Expanse' })).toBeVisible();
 });
