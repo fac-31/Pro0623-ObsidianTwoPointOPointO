68d157420b1ca9de20afccb17f224c970a3bb798
nchua3012 <n.chua3012@googlemail.com>
2025-07-26 11:18:26 +0100

    update Python server and SvelteKit to pass user ID from session to build_world

--- src/routes/api/build/+server.ts
+++ src/routes/api/build/+server.ts
@@ -1,18 +1,25 @@
 import { json } from '@sveltejs/kit';
-import type { RequestHandler } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
 
-export const POST: RequestHandler = async ({ request }) => {
-	const { worldId } = await request.json();
-	console.log('Received build request for world ID (API):', worldId);
+export const POST: RequestHandler = async (event) => {
+	const { world_id } = await event.request.json();
+	const session = await event.locals.getSession();
+
+	if (!session?.user?.id) {
+		return json({ message: 'Unauthorized' }, { status: 401 });
+	}
+
+	const user_id = session.user.id;
+
+	console.log('building world', { world_id, user_id });
+
 	const res = await fetch('http://localhost:8001/api/build', {
 		method: 'POST',
-		body: JSON.stringify({
-			world_id: worldId
-		}),
 		headers: {
 			'Content-Type': 'application/json'
-		}
+		},
+		body: JSON.stringify({ world_id, user_id })
 	});
-	const data = await res.json();
-	return json(data, { status: 201 });
+
+	return res;
 };
